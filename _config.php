23544642da6139dddef12960bb152d2304f3bf4d
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-01-06 15:07:54 +1300

    FIX: fixing various

--- _config.php
+++ _config.php
@@ -3,9 +3,3 @@
 use SilverStripe\Admin\CMSMenu;
 use SilverStripe\Core\Config\Config;
 use Sunnysideup\DashboardWelcomeQuicklinks\Admin\DashboardWelcomeQuicklinks;
-
-if (0 === strpos($_SERVER['REQUEST_URI'], '/admin/')) {
-    if (Config::inst()->get(DashboardWelcomeQuicklinks::class, 'hide_from_menu')) {
-        CMSMenu::remove_menu_class(DashboardWelcomeQuicklinks::class);
-    }
-}
